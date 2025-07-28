76a27a7943b5cd2ce18de1431602f66392772164
mengjunsu <mengjunsu1@gmail.com>
2025-07-28 15:23:54 +0800

    feat: count requests exporter

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by Ping handler",
+	})
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	pingCounter.Inc()
+	fmt.Fprintf(w, "pong")
+}
+
+func main() {
+	prometheus.MustRegister(pingCounter)
+
+	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8989", nil)
+}
